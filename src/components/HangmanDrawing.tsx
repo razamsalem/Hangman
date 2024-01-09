171d2e7d4180a93a34941fcc84cb3a266fc40de8
Raz <amsalemraz10@gmail.com>
2024-01-09 16:09:15 +0200

    The hanging man built

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,9 +1,73 @@
+const HEAD = (
+    <div className="
+    w-12 h-12 
+    rounded-full
+    border-8 border-solid border-black 
+    absolute top-12 right-[-20px]
+    "/>
+)
+
+const BODY = (
+    <div className="
+    w-2.5 h-24 
+    bg-black
+    absolute top-24 right-0"
+    />
+)
+
+const RIGHT_ARM = (
+    <div className="
+    w-24 h-2.5 
+    bg-black
+    absolute top-32 right-[-96px]
+    rotate-[-30deg]
+    origin-bottom-left
+    "/>
+)
+
+const LEFT_ARM = (
+    <div className="
+    w-24 h-2.5 
+    bg-black
+    absolute top-32 right-2.5
+    rotate-[30deg]
+    origin-bottom-right
+    "/>
+)
+
+const RIGHT_LEG = (
+    <div className="
+    w-24 h-2.5 
+    bg-black
+    absolute top-44 right-[-86px]
+    rotate-[60deg]
+    origin-bottom-left
+    "/>
+)
+
+const LEFT_LEG = (
+    <div className="
+    w-24 h-2.5 
+    bg-black
+    absolute top-44 right-0
+    rotate-[-60deg]
+    origin-bottom-right
+    "/>
+)
+
 export function HangmanDrawing() {
+
     return (
         <div className="relative">
-            <div className="h-12 w-2.5 absolute top-0 right-0 bg-black"/>
-            <div className="h-2.5 w-52 ml-32 bg-black"/>
-            <div className="h-96 w-2.5 ml-32 bg-black"/>
+            {HEAD}
+            {BODY}
+            {RIGHT_ARM}
+            {LEFT_ARM}
+            {RIGHT_LEG}
+            {LEFT_LEG}
+            <div className="h-12 w-2.5 absolute top-0 right-0 bg-black" />
+            <div className="h-2.5 w-52 ml-32 bg-black" />
+            <div className="h-96 w-2.5 ml-32 bg-black" />
             <div className="h-2.5 w-64 bg-black" />
         </div>
     )
