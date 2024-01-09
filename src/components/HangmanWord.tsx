8124b8669bfa77b48a1e5cd4136939918cced2c9
Raz <amsalemraz10@gmail.com>
2024-01-09 20:35:40 +0200

    When the user has chosen a correct letter it will appear on the screen

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,11 +1,18 @@
 export function HangmanWord() {
-    const word = 'test'
+    const word = "test"
+    const guessedLetters = ["t" , 's']
 
     return (
         <div className="flex gap-1 text-8xl font-bold uppercase">
             {word.split('').map((letter, idx) => (
-                <span className="m-2 border-b-[.1em] border-black">
-                    <span >{letter}</span>
+                <span className="m-2 border-b-[.1em] border-black" key={idx}>
+                    <span style={{
+                        visibility: guessedLetters.includes(letter.toLowerCase())
+                            ? "visible"
+                            : "hidden",
+                    }}>
+                        {letter}
+                    </span>
                 </span>
             ))}
         </div>
