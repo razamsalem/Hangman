4ad1361237aac49802e39e6b4f31ea6018528fe4
Raz <amsalemraz10@gmail.com>
2024-01-09 15:09:50 +0200

    Adding tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import { useState } from "react";
 import words from "./wordList.json"
+// import './index.css'
+
+import { Keyboard } from "./components/Keyboard";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
-  
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
   return (
-    <h1>Initial</h1>
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: '2rem',
+      margin: "0 auto",
+      alignItems: "center"
+    }}>
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      {/* <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard /> */}
+
+    </div>
   )
 }
 
