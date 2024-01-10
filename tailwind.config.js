2603de069d30d0ea0f99ce8565ec39df72a46335
Raz <amsalemraz10@gmail.com>
2024-01-10 12:49:15 +0200

    Adding custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#19A7CE",
+        "light": "#BFCFE7",
+        "lighter": "#F8EDFF"
+      }
+    },
   },
   plugins: [],
 }
