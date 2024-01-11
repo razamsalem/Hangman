dfffd6c9c9b197b17046c98cab064febaa52f66d
Raz <amsalemraz10@gmail.com>
2024-01-11 17:07:34 +0200

    Add more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
     extend: {
       colors: {
         "primary": "#19A7CE",
+        "secondary": "#f6ad55",
+        "secondary-light": "#e9ba80",
         "darker": "#232323",
         "light": "#BFCFE7",
         "lighter": "#F8EDFF"
