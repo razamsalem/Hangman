d8c6aa0eb20d62f8b1f1f2ad8e688b8c910b61b6
Raz <amsalemraz10@gmail.com>
2024-01-11 16:45:34 +0200

    Dark Mode feature implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router'
 import { Home } from './pages/Home'
 import { Hangman } from './pages/Hangman'
+import { useState } from 'react'
 
 function App() {
-
+  const [isDarkMode, setDarkMode] = useState(true)
   return (
     <div>
-      <main className='font-nunito bg-darker text-primary min-h-screen'>
+      <main className={`font-nunito ${isDarkMode ? 'bg-darker text-primary' : 'bg-slate-300 text-orange-500'} min-h-screen`}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/play" element={<Hangman />} />
+          <Route path="/" element={<Home isDarkMode={isDarkMode} setDarkMode={setDarkMode} />} />
+          <Route path="/play" element={<Hangman isDarkMode={isDarkMode} />} />
           {/* <Route path="/toy" element={<ToyIndex />} /> */}
           {/* <Route path="/toy/:toyId" element={<ToyDetails />} /> */}
           {/* <Route path="/toy/edit/:toyId" element={<ToyEdit />} /> */}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,45 @@
 import { useNavigate } from 'react-router-dom'
 import darkModeHangman from '../assets/img/dark-hangman.svg'
+import { useState } from 'react'
 
-export function Home() {
+type HomeProps = {
+    isDarkMode: boolean
+    setDarkMode: (value: boolean) => void
+}
+
+export function Home({ isDarkMode, setDarkMode }: HomeProps) {
+    const [isModalOpen, setModalOpen] = useState(false)
+
     const navigate = useNavigate()
+    const gradientColors = isDarkMode
+        ? 'from-cyan-500 to-blue-500'
+        : 'from-pink-500 to-orange-400';
 
     function navigateGame() {
         navigate('/play')
     }
 
+    function openModal() {
+        setModalOpen(true)
+    }
+
+    function closeModal() {
+        setModalOpen(false)
+    }
+
+    function toggleDarkMode() {
+        setDarkMode(!isDarkMode)
+      }
+
     return (
         <section>
-            <div className="text-center text-4xl font-regular py- ">
-                <h1 className="pt-14">Hangman</h1>
-                <p className="py-4 font-thin">The classic word game</p>
+            <div className="text-center text-4xl font-regular py-">
+                <h1 className="pt-14">
+                    <span className={`bg-gradient-to-r ${gradientColors} text-transparent bg-clip-text`}>Hangman</span>
+                </h1>
+                <p className="py-4 font-thin">
+                    <span className={`bg-gradient-to-r ${gradientColors} text-transparent bg-clip-text`}>The classic word game</span>
+                </p>
             </div>
 
             <div className='w-max mx-auto'>
@@ -20,10 +47,24 @@ export function Home() {
             </div>
 
             <div className='flex flex-col items-center space-y-6'>
-                <button onClick={navigateGame} className='btn mt-7 mb-0'>Play</button>
-                <button className='btn'>Languages</button>
-                <button className='btn'>Settings</button>
+                <button onClick={navigateGame} className={`${isDarkMode ? 'btn-dark' : 'btn-light'} mt-7 mb-0`}>Play</button>
+                <button className={`${isDarkMode ? 'btn-dark' : 'btn-light'} `}>Languages</button>
+                <button onClick={openModal} className={`${isDarkMode ? 'btn-dark' : 'btn-light'} `}>Settings</button>
             </div>
+
+            {isModalOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className=" relative bg-white p-6 rounded-lg">
+                        <p className="mb-4 text-center">Toggle Dark Mode</p>
+                        <button onClick={toggleDarkMode} className={`${isDarkMode ? 'btn-light' : 'btn-dark'}`}>
+                            {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+                        </button>
+                        <button onClick={closeModal} className="absolute top-[-8px] right-[-8px]">
+                            <span className='w-8 h-8 bg-slate-300 rounded-full flex items-center justify-center'><span className={`fa-solid fa-x ${isDarkMode ? 'text-primary' : 'text-orange-500'}`}></span></span>
+                        </button>
+                    </div>
+                </div>
+            )}
 
         </section>
     )
